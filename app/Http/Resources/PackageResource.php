d9c7683cd50ff108f0007f416e98f4c5b3937039
Christian Meude <christianmeude17@gmail.com>
2026-08-13 17:10:27 +0800

    chore: revert API band-aids and update agent protocols

--- app/Http/Resources/PackageResource.php
+++ app/Http/Resources/PackageResource.php
@@ -14,16 +14,16 @@ class PackageResource extends JsonResource
             'name' => $this->name,
             'description' => $this->description,
             'inclusions' => $this->inclusions ?? [],
-            'pax_options' => array_map('intval', $this->pax_options ?? []),
+            'pax_options' => $this->pax_options ?? [],
             'freebies' => $this->freebies ?? [],
             'price' => (float) $this->price,
             'rating' => (float) $this->rating,
             'reviews_count' => (int) $this->reviews_count,
             'images' => $this->images ?? [],
             'gallery_images' => $this->gallery_images ?? [],
-            'scents' => ScentResource::collection($this->whenLoaded('scents') ?? []),
-            'created_at' => (string) ($this->created_at ?? ''),
-            'updated_at' => (string) ($this->updated_at ?? ''),
+            'scents' => ScentResource::collection($this->whenLoaded('scents')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
         ];
     }
 }
