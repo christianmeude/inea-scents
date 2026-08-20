cdf28cf783aecb6b74b815e62ea077567accf481
Christian Meude <christianmeude17@gmail.com>
2026-08-21 05:18:45 +0800

    Test CI: add optional status parameter to bookings

--- app/Http/Controllers/Api/BookingController.php
+++ app/Http/Controllers/Api/BookingController.php
@@ -24,6 +24,12 @@ class BookingController extends Controller
         security: [['sanctum' => []]],
         tags: ['Bookings']
     )]
+    #[OAT\QueryParameter(
+        name: 'status',
+        description: 'Filter bookings by status',
+        required: false,
+        schema: new OAT\Schema(type: 'string')
+    )]
     #[OAT\Response(
         response: 200,
         description: 'Successful operation',
