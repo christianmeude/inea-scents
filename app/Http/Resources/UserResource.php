7ce822537bd7a6f724ef961aa69b1941eee592a1
Christian Meude <christianmeude17@gmail.com>
2026-08-22 12:43:32 +0800

    fix: cast is_admin to boolean in API response

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -13,7 +13,7 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
-            'is_admin' => $this->is_admin,
+            'is_admin' => (bool) $this->is_admin,
         ];
     }
 }
